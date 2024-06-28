b8c30b1eead934be6ac909f08c079b80d76dd883
avtotrainer <trainer@din.ge>
2024-06-28 11:43:08 +0400

    lsp config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,18 @@ return {
       "TmuxNavigatePrevious",
     },
   },
-
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
